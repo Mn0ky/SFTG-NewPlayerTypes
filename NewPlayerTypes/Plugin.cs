4cf84bbcf6694910c0957ca7b746142094e2d148
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2022-08-04 18:15:55 -0400

    - Added SimpleAntiCheat dependency.

--- NewPlayerTypes/Plugin.cs
+++ NewPlayerTypes/Plugin.cs
@@ -3,39 +3,39 @@ using BepInEx;
 using HarmonyLib;
 using NewPlayerTypes.Patches;
 
-namespace NewPlayerTypes
+namespace NewPlayerTypes;
+
+[BepInPlugin(Guid, Name, Version)]
+[BepInDependency("monky.plugins.SimpleAntiCheat")]
+[BepInProcess("StickFight.exe")]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(Guid, Name, Version)]
-    [BepInProcess("StickFight.exe")]
-    public class Plugin : BaseUnityPlugin
-    {
-        private const string Guid = "monky.plugins.NewPlayerClasses";
-        private const string Name = "NewPlayerClasses";
-        private const string Version = "1.0.0";
+    private const string Guid = "monky.plugins.NewPlayerClasses";
+    private const string Name = "NewPlayerClasses";
+    private const string Version = "1.0.0";
         
-        private void Awake()
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {Guid} is loaded!");
+        try
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {Guid} is loaded!");
-            try
-            {
-                var harmony = new Harmony(Guid);
+            var harmony = new Harmony(Guid);
                 
-                Logger.LogInfo("Applying MultiplayerManager patches...");
-                MultiplayerManagerPatches.Patches(harmony);
-                Logger.LogInfo("Applying GameManager patch...");
-                GameManagerPatch.Patch(harmony);
-                Logger.LogInfo("Applying SetMovementAbility patch...");
-                SetMovementAbilityPatch.Patch(harmony);
-                Logger.LogInfo("Applying ControllerHandler patch...");
-                ControllerHandlerPatch.Patch(harmony);
-                Logger.LogInfo("Applying DisableIfPlayed patch...");
-                TimeEventPatch.Patch(harmony);
-            }
-            catch (Exception e)
-            {
-                Logger.LogInfo(e);
-            }
+            Logger.LogInfo("Applying MultiplayerManager patches...");
+            MultiplayerManagerPatches.Patches(harmony);
+            Logger.LogInfo("Applying GameManager patch...");
+            GameManagerPatch.Patch(harmony);
+            Logger.LogInfo("Applying SetMovementAbility patch...");
+            SetMovementAbilityPatch.Patch(harmony);
+            Logger.LogInfo("Applying ControllerHandler patch...");
+            ControllerHandlerPatch.Patch(harmony);
+            Logger.LogInfo("Applying DisableIfPlayed patch...");
+            TimeEventPatch.Patch(harmony);
+        }
+        catch (Exception e)
+        {
+            Logger.LogInfo(e);
         }
     }
 }
